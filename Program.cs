9019ed10a36e4b7e523af0662a2d9c2061b81be0
Marcel Bornancin <dolly.marcel@gmail.com>
2019-08-04 19:14:36 -0300

    PrintOperationDelegate example

--- Program.cs
+++ Program.cs
@@ -7,6 +7,24 @@ namespace DotNetPlayground
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            new Program().Run();
         }
+        delegate void PrintOperationDelegate<T1, T2>(T1 a, T2 b);
+
+        PrintOperationDelegate<int, int> IntegerOperation;
+
+        private void PrintIntegerSum(int a, int b) 
+            => Console.WriteLine($"{a}+{b}={a + b}");
+        
+        private void PrintIntegerMultiplication(int a, int b) 
+            => Console.WriteLine($"{a}*{b}={a * b}");
+
+        private void Run()
+        {
+            IntegerOperation += new PrintOperationDelegate<int, int>(PrintIntegerSum);
+            IntegerOperation += PrintIntegerMultiplication;
+            IntegerOperation?.Invoke(3, 3);
+        }
+
     }
 }
