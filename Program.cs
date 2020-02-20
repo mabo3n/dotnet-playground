ddb1f72f57df6b143cda71d4ec1fc122a1b07890
Marcel <dolly.marcel@gmail.com>
2020-02-19 23:24:45 -0300

    Load giant string-long dictionary to measure the app's memory usage

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DotNetPlayground
 {
@@ -7,6 +9,40 @@ namespace DotNetPlayground
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            new Program().Run();
+        }
+
+        public void Run()
+        {
+            var frequenciesIndex = new Dictionary<string, long>();
+            var uniqueWordCount = 3_893_941; // ~350MB
+
+            var random = new Random();
+            var timer = Stopwatch.StartNew();
+            for (var i = 0; i < uniqueWordCount; i++)
+            {
+                var @string = i.ToString();
+                var randomOffset = LongRandom(10, long.MaxValue, random);
+
+                frequenciesIndex[@string] = randomOffset;
+            }
+
+            Console.WriteLine($"Index loaded in: {timer.Elapsed}");
+
+            string input;
+            while ((input = Console.ReadLine()) != "tchau")
+            {
+                Console.WriteLine($"Offset for word '{input}' is {frequenciesIndex[input]}");
+            }
+        }
+
+        long LongRandom(long min, long max, Random rand)
+        {
+            byte[] buf = new byte[8];
+            rand.NextBytes(buf);
+            long longRand = BitConverter.ToInt64(buf, 0);
+
+            return (Math.Abs(longRand % (max - min)) + min);
         }
     }
 }
